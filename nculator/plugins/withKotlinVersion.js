33f03f9507088589cd2855763c6ee839abe60ae3
Soumya001 <soumayamojumdar0198@gmail.com>
2026-06-22 10:47:25 +0530

    fix: force Kotlin 1.9.25 via string replace + fix -P freeCompilerArgs split

--- nculator/plugins/withKotlinVersion.js
+++ nculator/plugins/withKotlinVersion.js
@@ -1,22 +1,33 @@
 const { withProjectBuildGradle } = require('@expo/config-plugins');
 
-const SUPPRESS_FLAG =
-  '-P plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=true';
+module.exports = (config) =>
+  withProjectBuildGradle(config, (config) => {
+    let contents = config.modResults.contents;
 
-const ALLPROJECTS_BLOCK = `
+    // Upgrade Kotlin from 1.9.24 → 1.9.25 (Compose Compiler 1.5.15 requires 1.9.25)
+    if (contents.includes('kotlinVersion = "1.9.24"')) {
+      contents = contents.replace(
+        'kotlinVersion = "1.9.24"',
+        'kotlinVersion = "1.9.25"'
+      );
+    }
+
+    // Belt-and-suspenders: suppress the version check as well
+    if (!contents.includes('suppressKotlinVersionCompatibilityCheck')) {
+      contents += `
 allprojects {
     tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile).configureEach {
         kotlinOptions {
-            freeCompilerArgs += ["${SUPPRESS_FLAG}"]
+            freeCompilerArgs += [
+                "-P",
+                "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=true"
+            ]
         }
     }
 }
 `;
-
-module.exports = (config) =>
-  withProjectBuildGradle(config, (config) => {
-    if (!config.modResults.contents.includes('suppressKotlinVersionCompatibilityCheck')) {
-      config.modResults.contents += ALLPROJECTS_BLOCK;
     }
+
+    config.modResults.contents = contents;
     return config;
   });
